31a23bb5a403e16b0133126d79b79cdcb0820223
rj-roy <dpjdeveloper.me@gmainl.com>
2026-06-10 20:43:16 +0600

    feat:index

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        const db = await client.db(process.env.DB_NAME);
+        const jobsCollection = await db.collection(process.env.JOBS_COLLECTION);
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+    } finally {
+        // await client.close();
+    }
+};
+
+run().catch(console.dir);
+app.listen(PORT, (req, res) => {
+    console.log('running...!');
+});
